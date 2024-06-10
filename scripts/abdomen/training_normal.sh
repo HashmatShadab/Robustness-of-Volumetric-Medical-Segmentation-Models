c3572afd3f21aed2627828b83f084a922c2b4ef1
Malik Hashmat <hashmat.shadab.malik@gmail.com>
2024-06-10 12:26:18 +0400

    Refactor abdomen scripts and remove redundant ones
    Deleted two redundant scripts for abdomen attacks: `attack_freq_cospgd.sh` and `attack_only_cos_pgd.sh`. Also, made significant changes to `training_normal.sh` by removing

--- scripts/abdomen/training_normal.sh
+++ scripts/abdomen/training_normal.sh
@@ -1,81 +1,70 @@
 #!/bin/bash
 
 
-exp_num=$1
 
 DATA_DIR="datasets3d/abdomenCT_resampled"
 JSON_LIST="dataset.json"
-# Baseline Training of models on Abdomen-CT and Adv. Training (PGD-K, FGSM, GN, VAFA-3D) on Abdomen-CT
-
-
-if [ $exp_num -eq 1 ]
-then
-  echo "Running Natural Training on Abdomen-CT"
-    python training.py  --model_name unet --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results" \
-    --val_every 15
-
-    echo "Running Natural Training on Abdomen-CT"
-    python training.py  --model_name unetr --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results" \
-    --val_every 15
-
-    echo "Running Natural Training on Abdomen-CT"
-    python training.py  --model_name segresnet --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results" \
-    --val_every 15
-
-fi
-
-
-if [ $exp_num -eq 2 ]
-then
-  echo "Running Natural Training on Abdomen-CT"
-    python training.py  --model_name swin_unetr --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=1 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results" \
-    --val_every 15
-
-    echo "Running Natural Training on Abdomen-CT"
-    python training.py  --model_name umamba_bot --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results" \
-    --val_every 15
-
-
-fi
-
-
-if [ $exp_num -eq 3 ]
-then
-  echo "Running Natural Training on Abdomen-CT"
-    python training.py --noamp True  --model_name umamba_enc --in_channels 1 --out_channel 14 --feature_size=16 \
-    --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=1 \
-    --adv_training_mode False --freq_reg_mode False \
-    --attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-    --max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
-    --save_model_dir="./Results_no_amp" \
-    --val_every 15
+# Baseline Training of models on Abdomen-CT
 
 
 
-fi
+echo "Running Natural Training on Abdomen-CT for UNET"
+python training.py  --model_name unet --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results" \
+--val_every 15
+
+echo "Running Natural Training on Abdomen-CT for UNETR"
+python training.py  --model_name unetr --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results" \
+--val_every 15
+
+echo "Running Natural Training on Abdomen-CT for SegResNet"
+python training.py  --model_name segresnet --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results" \
+--val_every 15
+
+
+
+
+echo "Running Natural Training on Abdomen-CT for Swin-UNETR"
+python training.py  --model_name swin_unetr --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=1 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results" \
+--val_every 15
+
+echo "Running Natural Training on Abdomen-CT for UMAMBA-BOT"
+python training.py  --model_name umamba_bot --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=3 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results" \
+--val_every 15
+
+
+echo "Running Natural Training on Abdomen-CT for UMAMBA-ENC"
+python training.py --noamp True  --model_name umamba_enc --in_channels 1 --out_channel 14 --feature_size=16 \
+--dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --batch_size=1 \
+--adv_training_mode False --freq_reg_mode False \
+--attack_name vafa-3d --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
+--max_epochs 5000 --optim_lr=1e-4 --lrschedule=warmup_cosine --infer_overlap=0.5 \
+--save_model_dir="./Results_no_amp" \
+--val_every 15
+
+
 
--- scripts/abdomen/transferability.sh
+++ scripts/abdomen/transferability.sh
@@ -11,48 +11,6 @@ model_names=("unetr" "swin_unetr" "segresnet" "umamba_bot" "umamba_enc" "unet")
 echo "==========================================================================================="
 echo "==========================================================================================="
 
-echo "********************************Running Attack on Abdomen CT*********************************"
-
-
-for ((i=0; i<6; i++)); do
-
-   model_name=${model_names[$i]}
-   ckpt_path=${ckpt_paths[$i]}
-
-   echo "==========================================================================================="
-   echo "Loading Model $model_name with Checkpoint $ckpt_path"
-
-   # check if the checkpoint path exists
-    if [ ! -f $ckpt_path ]; then
-      echo "Checkpoint path $ckpt_path does not exist. Skipping..."
-      continue
-    fi
-
-
-  echo "CosPGD Attack on Abdomen CT with PGD eps 4 $model_name"
-  python wb_attack.py  --model_name $model_name   --in_channels 1 --out_channel 14 --feature_size=16 --checkpoint_path $ckpt_path \
-  --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --slice_batch_size $SLICE_BS  \
-  --attack_name cospgd --eps 4 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-
-  echo "-------------------------------------------------------------------------------------------"
-
-
-  echo "CosPGD Attack on Abdomen CT with PGD eps 8 $model_name"
-  python wb_attack.py  --model_name $model_name   --in_channels 1 --out_channel 14 --feature_size=16 --checkpoint_path $ckpt_path \
-  --dataset abdomen --data_dir=$DATA_DIR --json_list $JSON_LIST --slice_batch_size $SLICE_BS  \
-  --attack_name cospgd --eps 8 --q_max 20 --steps 20 --block_size 32 32 32 --use_ssim_loss False \
-
-  echo "-------------------------------------------------------------------------------------------"
-
-
-
-  done
-
-
-# use echo to have a clear separation between different experiments
-
-echo "==========================================================================================="
-echo "==========================================================================================="
 echo "********************************Running Inference on Abdomen CT*********************************"
 
 
@@ -88,7 +46,7 @@ for ((j=0; j<6; j++)); do
   fi
 
   # Loop over all the folders in parent directory starting with "Adv"
-  for adv_dir in $surrogate_parent_dir/Adv*/abdomen/*cospgd*; do
+  for adv_dir in $surrogate_parent_dir/Adv*/abdomen/*; do
     # run code only if the folder exists
     if [ -d "$adv_dir" ]; then
 
@@ -103,4 +61,5 @@ for ((j=0; j<6; j++)); do
     done
   done
 done
+
 
